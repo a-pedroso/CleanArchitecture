3810f80bd85e92e6474f30fa53120296b9c3ec13
a-pedroso <andre.pedroso@ceiia.com>
2021-02-17 22:53:56 +0000

    add loki and grafana, loki serilog sink, update migrations

--- src/Infrastructure/Persistence/Migrations/20210217225046_Init.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20210217225046_Init.Designer.cs
@@ -4,14 +4,16 @@ using CleanArchitecture.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArchitecture.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210217225046_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
